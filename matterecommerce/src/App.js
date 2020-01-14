adac09424685fcd29640ec5e94ba26437231505c
coatriver <sylvain.jacot@hotmail.be>
2020-01-14 13:11:40 +1100

    mock navigation created v1 raw

--- matterecommerce/src/App.js
+++ matterecommerce/src/App.js
@@ -1,10 +1,29 @@
 import React from 'react';
-import './reset.css';
+//import './reset.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello World!</h1>
+        <ul>
+            <li>shop
+                <ul>
+                    <li>apparels
+                        <ul>
+                            <li>pants
+                                <ul>
+                                    <li>last 1</li>
+                                    <li>last 2</li>
+                                </ul>
+                            </li>
+                            <li>jumpsuits</li>
+                        </ul>
+                    </li>
+                    <li>accessoires</li>
+                </ul>
+            </li>
+
+        </ul>
     </div>
   );
 }
---
+++ matterecommerce/src/constant/navigation.js
@@ -0,0 +1,38 @@
+export const navigationListItems =  {
+navigation: [
+        {
+            navlabel: 'shop',
+            nestednav:
+            {
+            category: 'Apparels',
+                subcategory: [
+                    {
+                        subcategoryName : 'pants',
+                        subcategoryType : [
+                            {
+                                subcategoryTypeName : 'sidewept',
+                            },
+                        ]
+                    },
+                ]
+            },
+        },
+        {
+            navlabel: 'shop2',
+            nestednav:
+            {
+            category: 'Apparels',
+                subcategory: [
+                    {
+                        subcategoryName : 'pants',
+                        subcategoryType : [
+                            {
+                                subcategoryTypeName : 'sidewept',
+                            },
+                        ]
+                    },
+                ]
+            },
+        },
+    ]
+}
